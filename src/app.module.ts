7b12c2bbf00efc28243ea2679e08959b3b218834
ike-Njoku <ikenjokudc@gmail.com>
2021-10-15 21:30:12 +0100

    Updates

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnumeratorModule } from './enumerator/enumerator.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [EnumeratorModule],
+  imports: [
+    EnumeratorModule,
+    MongooseModule.forRoot('mongodb://localhost/project-crave')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
