28497ca377aaa213a51afb2660442b541af71283
ike-Njoku <ikenjokudc@gmail.com>
2021-10-28 19:48:15 +0100

    Updated port

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const port = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:4200', 'http://localhost:4201']
   });
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
